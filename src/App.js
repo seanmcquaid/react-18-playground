366e97bb629ddfb8bf6b42a692c706b6988c77c3
seanmcquaid <sean.mcquaid2@verizon.net>
2021-09-15 20:16:18 -0400

    added auto batching for normal set state events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
+  const renderCount = useRef(0);
+
+  function handleClick() {
+    setCount((c) => c + 1); // Does not re-render yet
+    setFlag((f) => !f); // Does not re-render yet
+    // React will only re-render once at the end (that's batching!)
+  }
+
+  console.log('render', renderCount.current++);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Next</button>
+      <h1 style={{ color: flag ? 'blue' : 'black' }}>{count}</h1>
     </div>
   );
 }
