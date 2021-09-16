2214abacd223a1d11960a44facf2993700f41a16
seanmcquaid <sean.mcquaid2@verizon.net>
2021-09-15 20:22:01 -0400

    added batching example with react 18

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { useRef, useState } from 'react';
+import { flushSync } from 'react-dom';
+
+function fetchSomething() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log('fetching');
+    }, 3000);
+  });
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,10 +15,25 @@ function App() {
   const renderCount = useRef(0);
 
   function handleClick() {
-    setCount((c) => c + 1); // Does not re-render yet
-    setFlag((f) => !f); // Does not re-render yet
-    // React will only re-render once at the end (that's batching!)
+    fetchSomething().then(() => {
+      // React 18 and later DOES batch these:
+      setCount((c) => c + 1);
+      setFlag((f) => !f);
+      // React will only re-render once at the end (that's batching!)
+    });
   }
+
+  // flushSync can be used to explicitly avoid vatching if we need to read something from the DOM immediately but this is a rare use case
+  // function handleClick() {
+  //   flushSync(() => {
+  //     setCount((c) => c + 1);
+  //   });
+  //   // React has updated the DOM by now
+  //   flushSync(() => {
+  //     setFlag((f) => !f);
+  //   });
+  //   // React has updated the DOM by now
+  // }
 
   console.log('render', renderCount.current++);
 
