a34c9500a76c7b9c4e95c6078b08d68b9431cb65
seanmcquaid <sean.mcquaid2@verizon.net>
2021-09-19 16:46:46 -0400

    added example for start transition

--- src/Transition.js
+++ src/Transition.js
@@ -1,23 +1,41 @@
 import { useState, useTransition } from 'react';
 
+const getUrl = (term) =>
+  `https://itunes.apple.com/search?term=${term}&country=us`;
+
 const Transition = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [isPending, startTransition] = useTransition({
-    timeoutMs: 3000,
+    timeoutMs: 5000,
   });
 
   const onChange = (event) => {
     const value = event.target.value;
     setQuery(value);
     startTransition(() => {
-      fetch().then(() => {
-        setResults([]);
+      fetch(getUrl(value)).then((response) => {
+        response.json().then((json) => {
+          setResults(json.results);
+        });
       });
     });
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <input value={query} onChange={onChange} />
+      {isPending && <p>Pending</p>}
+      <ul>
+        {results.map((result, i) => (
+          <li key={i}>
+            <h4>{result.trackName}</h4>
+            <p>{result.artistName}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Transition;
